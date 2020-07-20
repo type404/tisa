91e4f4a84a76d4048a4309528eb5e05a1f6f6e50
Tisa Majumder <coderbychoice01@gmail.com>
2020-07-20 11:19:46 +1000

    Updates

---
+++ precache-manifest.8da4b6b9b4131b5594d2bb9925fe01c1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c47300966feccb7b560da7afa98714b1",
+    "url": "/tisa/index.html"
+  },
+  {
+    "revision": "6b623860876474063d9c",
+    "url": "/tisa/static/css/main.126c14d6.chunk.css"
+  },
+  {
+    "revision": "46ff7e3c174a9926d567",
+    "url": "/tisa/static/js/2.00aedd60.chunk.js"
+  },
+  {
+    "revision": "d09ac505fec0e9130a1d5b85fb3fa6dc",
+    "url": "/tisa/static/js/2.00aedd60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b623860876474063d9c",
+    "url": "/tisa/static/js/main.cf89bcbc.chunk.js"
+  },
+  {
+    "revision": "55615a85b2b6a65ce16d",
+    "url": "/tisa/static/js/runtime-main.2cb8a6b6.js"
+  },
+  {
+    "revision": "b94bf7c8e2bccfd4db1bc0f3fe01f05f",
+    "url": "/tisa/static/media/profile-bg.b94bf7c8.jpg"
+  },
+  {
+    "revision": "dc087e6f50f2dda613b5735dff689740",
+    "url": "/tisa/static/media/tisa.dc087e6f.jpg"
+  }
+]);
